3d2099983adc0a3bfb7412bf30afd671d733c407
Zakhar <lephiziel@gmail.com>
2026-06-04 01:34:01 +0500

    feat: crud for subjects

---
+++ backend/internal/task/model.go
@@ -0,0 +1,18 @@
+package task
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Task struct {
+	gorm.Model
+	UserID      uint       `json:"user_id" gorm:"not null"`
+	SubjectID   *uint      `json:"subject_id" gorm:"not null"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	DueDate     *time.Time `json:"due_date"`
+	Priority    string     `json:"priority" gorm:"not null;default:medium"`
+	Status      string     `json:"status" gorm:"not null;default:todo"`
+}
